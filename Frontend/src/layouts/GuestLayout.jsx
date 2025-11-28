e588eff657b396ed808e9064c0cb1d63c2fac4ff
m3gezz <m3gezz@gmail.com>
2025-11-28 18:03:57 +0100

    shadcn added

--- Frontend/src/layouts/GuestLayout.jsx
+++ Frontend/src/layouts/GuestLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../slices/guest/Header";
 
 export default function GuestLayout() {
   return (
     <>
-      GuestLayout
+      <Header />
       <Outlet />
     </>
   );
--- Frontend/src/layouts/UserLayout.jsx
+++ Frontend/src/layouts/UserLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../slices/user/Header";
 
 export default function UserLayout() {
   return (
     <>
-      UserLayout
+      <Header />
       <Outlet />
     </>
   );
